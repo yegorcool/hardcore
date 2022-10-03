77a59bb6435de389b9edba7f79ec586b20b5cb8e
EgorV <vasilyevyegor@gmail.com>
2022-10-03 12:24:11 +0300

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Hardcore FC</title>
+
+        <link rel="shortcut icon" href="hardcore-fight-icon.png" type="image/x-icon">
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
